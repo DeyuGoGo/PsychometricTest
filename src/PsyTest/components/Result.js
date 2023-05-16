5b2cfb5f463f1bebe090e39149d1105031c1cf5c
DeyuPc <a0924xxx@gmail.com>
2023-05-16 21:36:39 +0800

    update

--- src/PsyTest/components/Result.js
+++ src/PsyTest/components/Result.js
@@ -13,25 +13,28 @@ function Result({ result }) {
   return (
     <div className="result-container">
       <h2>你的人格類型是：{result.name}</h2>
+      <img src={result.imageUrl} alt={result.name} className="result-image"/>
       <p>{result.description}</p>
       
       <div className="compatibility-container">
         <div className="easy-to-get-along">
           <h3>容易相處的人格類型：</h3>
-          <ul>
-            {result.easy_to_get_along_with.map((type) => (
-              <li key={type}>{type}</li>
-            ))}
-          </ul>
+          {result.easy_to_get_along_with.map((type) => (
+            <div key={type.name} className="compatibility-item">
+              <img src={type.imageUrl} alt={type.name} className="compatibility-image"/>
+              <p>{type.name}</p>
+            </div>
+          ))}
         </div>
         
         <div className="difficult-to-get-along">
           <h3>不容易相處的人格類型：</h3>
-          <ul>
-            {result.difficult_to_get_along_with.map((type) => (
-              <li key={type}>{type}</li>
-            ))}
-          </ul>
+          {result.difficult_to_get_along_with.map((type) => (
+            <div key={type.name} className="compatibility-item">
+              <img src={type.imageUrl} alt={type.name} className="compatibility-image"/>
+              <p>{type.name}</p>
+            </div>
+          ))}
         </div>
       </div>
 
